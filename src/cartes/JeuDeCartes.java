fcbf576331a7778f3f79f4df9c0a29e927953024
Noé Mastrorillo <noe.mastrorillo@gmail.com>
2025-03-06 08:11:15 +0100

    Fin TP1

--- src/cartes/JeuDeCartes.java
+++ src/cartes/JeuDeCartes.java
@@ -1,70 +1,58 @@
 package cartes;
 
 public class JeuDeCartes {
-	private Configuration[] typesDeCartes = {
-			new Configuration(new Borne(25), 10),
-			new Configuration(new Borne(50), 10),
-			new Configuration(new Borne(75), 10),
-			new Configuration(new Borne(100), 12),
-			new Configuration(new Borne(200), 4),
-			new Configuration(new Parade(Type.FEU), 14),
-			new Configuration(new FinLimite(), 6),
-			new Configuration(new Parade(Type.ESSENCE), 6),
-			new Configuration(new Parade(Type.CREVAISON), 6),
-			new Configuration(new Parade(Type.ACCIDENT), 6),
-			new Configuration(new Attaque(Type.FEU), 5),
-			new Configuration(new DebutLimite(), 4),
-			new Configuration(new Attaque(Type.ESSENCE), 3),
-			new Configuration(new Attaque(Type.CREVAISON), 3),
-			new Configuration(new Attaque(Type.ACCIDENT), 3),
-			new Configuration(new Botte(Type.FEU), 1),
-			new Configuration(new Botte(Type.ESSENCE), 1),
-			new Configuration(new Botte(Type.CREVAISON), 1),
-			new Configuration(new Botte(Type.ACCIDENT), 1),
-	};
-	
+	private Configuration[] typesDeCartes = { new Configuration(new Borne(25), 10),
+			new Configuration(new Borne(50), 10), new Configuration(new Borne(75), 10),
+			new Configuration(new Borne(100), 12), new Configuration(new Borne(200), 4),
+			new Configuration(new Parade(Type.FEU), 14), new Configuration(new FinLimite(), 6),
+			new Configuration(new Parade(Type.ESSENCE), 6), new Configuration(new Parade(Type.CREVAISON), 6),
+			new Configuration(new Parade(Type.ACCIDENT), 6), new Configuration(new Attaque(Type.FEU), 5),
+			new Configuration(new DebutLimite(), 4), new Configuration(new Attaque(Type.ESSENCE), 3),
+			new Configuration(new Attaque(Type.CREVAISON), 3), new Configuration(new Attaque(Type.ACCIDENT), 3),
+			new Configuration(new Botte(Type.FEU), 1), new Configuration(new Botte(Type.ESSENCE), 1),
+			new Configuration(new Botte(Type.CREVAISON), 1), new Configuration(new Botte(Type.ACCIDENT), 1), };
+
 	public Carte[] donnerCartes() {
 		int nbCartes = 0;
-		for (Configuration config: typesDeCartes) {
+		for (Configuration config : typesDeCartes) {
 			nbCartes += config.getNbExemplaires();
 		}
-		
+
 		Carte[] cartes = new Carte[nbCartes];
-		int i = 0;
-		for (Configuration config: typesDeCartes) {
-			for (int j = 0; j < config.getNbExemplaires(); j++) {
-				cartes[i+j] = config.getCarte();
+		for (int i = 0, j = 0; j < typesDeCartes.length; j++) {
+			Configuration config = typesDeCartes[j];
+			for (int k = 0; k < config.getNbExemplaires(); k++, i++) {
+				cartes[i] = config.getCarte();
 			}
-			i += config.getNbExemplaires();
 		}
-		
+
 		return cartes;
 	}
 
 	public String affichageJeuDeCartes() {
 		StringBuilder sb = new StringBuilder("JEU :\n\n");
-		for (Configuration config: typesDeCartes) {
-			sb.append(config.getNbExemplaires())
-				.append(' ')
-				.append(config.getCarte())
-				.append('\n');
+		for (Configuration config : typesDeCartes) {
+			sb.append(config.getNbExemplaires());
+			sb.append(' ');
+			sb.append(config.getCarte());
+			sb.append('\n');
 		}
 		return sb.toString();
 	}
-	
+
 	private static class Configuration {
 		private Carte carte;
 		private int nbExemplaires;
-		
-		public Configuration(Carte carte, int nbExemplaires) {
+
+		private Configuration(Carte carte, int nbExemplaires) {
 			this.carte = carte;
 			this.nbExemplaires = nbExemplaires;
 		}
-		
+
 		public Carte getCarte() {
 			return carte;
 		}
-		
+
 		public int getNbExemplaires() {
 			return nbExemplaires;
 		}
--- src/jeu/Sabot.java
+++ src/jeu/Sabot.java
@@ -10,26 +10,29 @@ public class Sabot implements Iterable<Carte> {
 	private int nbCartes;
 	private Carte[] cartes;
 	private int nbOperations = 0;
-	
+
 	public Sabot(Carte[] cartes) {
 		this.cartes = cartes;
 		this.nbCartes = cartes.length;
 	}
-	
+
 	public boolean estVide() {
 		return nbCartes == 0;
 	}
-	
+
 	public void ajouterCarte(Carte carte) {
 		cartes[nbCartes] = carte;
 		nbCartes++;
 		nbOperations++;
 	}
-	
+
 	public Carte piocher() {
 		Iterator<Carte> it = iterator();
-		Carte carte = it.next();
-		it.remove();
+		Carte carte = null;
+		if (it.hasNext()) {
+			carte = it.next();
+			it.remove();
+		}
 		return carte;
 	}
 
@@ -37,23 +40,26 @@ public class Sabot implements Iterable<Carte> {
 	public Iterator<Carte> iterator() {
 		return new Iterateur();
 	}
-	
+
 	private class Iterateur implements Iterator<Carte> {
 		private int indiceIterateur = 0;
 		private int nbOperationsReference = nbOperations;
 		private boolean nextEffectue = false;
-		
+
 		private void verifierConcurrence() {
 			if (nbOperations != nbOperationsReference) {
 				throw new ConcurrentModificationException();
 			}
 		}
-		
+
+		@Override
 		public boolean hasNext() {
 			return indiceIterateur < nbCartes;
 		}
-		
+
+		@Override
 		public Carte next() {
+			verifierConcurrence();
 			if (hasNext()) {
 				nextEffectue = true;
 				return cartes[indiceIterateur++];
@@ -61,14 +67,16 @@ public class Sabot implements Iterable<Carte> {
 				throw new NoSuchElementException();
 			}
 		}
-		
+
+		@Override
 		public void remove() {
+			verifierConcurrence();
 			if (!nextEffectue) {
 				throw new IllegalStateException();
 			}
-			
-			for (int i = indiceIterateur-1; i < nbCartes-1; i++) {
-				cartes[i] = cartes[i+1];
+
+			for (int i = indiceIterateur - 1; i < nbCartes - 1; i++) {
+				cartes[i] = cartes[i + 1];
 			}
 			nextEffectue = false;
 			indiceIterateur--;
